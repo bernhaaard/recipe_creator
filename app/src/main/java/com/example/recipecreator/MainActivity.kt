3e10a289fb3c51a15b88ef29b6ec6439c1fd834f
bernhard <berni.nops.kofler@gmail.com>
2024-01-25 18:21:17 +0100

    refactor: clean up AddRecipeScreen to make it usable and readable & add db and view-model instance

--- app/src/main/java/com/example/recipecreator/MainActivity.kt
+++ app/src/main/java/com/example/recipecreator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.recipecreator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -10,12 +11,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
+import com.example.recipecreator.data.local.AppDatabase
+import com.example.recipecreator.data.repository.RecipeRepository
 import com.example.recipecreator.ui.screens.NavGraphs
 import com.example.recipecreator.ui.theme.RecipeWizardTheme
+import com.example.recipecreator.ui.viewmodels.AddRecipeViewModel
 import com.ramcosta.composedestinations.DestinationsNavHost
 
-@Suppress("ktlint:standard:function-naming")
 class MainActivity : ComponentActivity() {
+    private val db by lazy {
+        Room.databaseBuilder(this, AppDatabase::class.java, "database.db").build()
+    }
+    private val addRecipeViewModel by viewModels<AddRecipeViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return AddRecipeViewModel(RecipeRepository(db.recipeDao())) as T
+                }
+            }
+        },
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ app/src/main/java/com/example/recipecreator/model/Ingredient.kt
@@ -0,0 +1,7 @@
+package com.example.recipecreator.model
+
+data class Ingredient(
+    val name: String,
+    val quantity: Double,
+    val unit: String,
+)
