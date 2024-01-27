43e99e178435042dda1efceabc2a6ec3679d379a
bernhard <berni.nops.kofler@gmail.com>
2024-01-28 00:42:08 +0100

    delete: delete UserViewState.kt

--- app/src/main/java/com/example/recipecreator/ui/viewstates/UserViewState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.recipecreator.ui.viewstates
-
-import com.example.recipecreator.model.User
-
-data class UserViewState(
-    val isLoading: Boolean = false,
-    val userInfo: User? = null,
-    val errorMessage: String? = null
-)
