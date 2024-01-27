6086d1b01988783ebfb9f931bebe00c27fb49b0f
cc221052 <cc221052@fhstp.ac.at>
2024-01-28 00:11:16 +0100

    Feat - Add Library cards to LibraryScreen(who are now clickable), Add DetailView For Recipes, Add EditScreen when clicked on "Edit Recipe"

---
+++ app/src/main/java/com/example/recipecreator/ui/screens/EditRecipeScreen.kt
@@ -0,0 +1,13 @@
+package com.example.recipecreator.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipeEditScreen() {
+    // Home Screen Composable
+    Column {
+        Text(text = "Home Screen")
+    }
+}
